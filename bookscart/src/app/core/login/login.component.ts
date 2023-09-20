c17be55b72d2dc69d438a3e9df151d7c624399f5
ClintonPeter98 <100680388+ClintonPeter98@users.noreply.github.com>
2023-09-20 18:15:21 +0530

    1 file modified

--- bookscart/src/app/core/login/login.component.ts
+++ bookscart/src/app/core/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment.development';
 import { loginmodel } from '../model/loginmodel';
 import { DataService } from '../../service/dataservice';
 
